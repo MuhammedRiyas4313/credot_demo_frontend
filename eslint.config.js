cc0a3c1d49c51a3dba130d7594f7c2bf00072fde
Muhammed-Riyas-ebslon <148744494+Muhammed-Riyas-ebslon@users.noreply.github.com>
2024-10-07 16:31:11 +0530

    Build error fixed

--- eslint.config.js
+++ eslint.config.js
@@ -4,20 +4,32 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
 
 export default tseslint
   .config(
-    { ignores: ["dist"] },
     {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      ignores: ["dist"],
+    },
+    {
+      extends: [
+        js.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+      ],
       files: ["**/*.{ts,tsx}"],
       languageOptions: {
         ecmaVersion: 2020,
         globals: globals.browser,
+        parserOptions: {
+          project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
+      settings: { react: { version: "18.3" } },
       plugins: {
         "react-hooks": reactHooks,
         "react-refresh": reactRefresh,
+        react,
       },
       rules: {
         ...reactHooks.configs.recommended.rules,
@@ -25,6 +37,8 @@ export default tseslint
           "warn",
           { allowConstantExport: true },
         ],
+        ...react.configs.recommended.rules,
+        ...react.configs["jsx-runtime"].rules,
       },
     }
   )
