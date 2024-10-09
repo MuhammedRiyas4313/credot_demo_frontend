234908946158b8763c835447ad9ba2a9f99a388a
Muhammed-Riyas-ebslon <148744494+Muhammed-Riyas-ebslon@users.noreply.github.com>
2024-10-10 02:33:25 +0530

    product list and brands

--- src/components/brandList/BrandSlide.tsx
+++ src/components/brandList/BrandSlide.tsx
@@ -4,7 +4,7 @@ import dummyBrand from "../../assets/images/dummy_product.webp";
 function BrandSlide({ el }: any) {
   return (
     <>
-      <div className="h-[49px] w-[49px] object-cover rounded-full overflow-hidden ">
+      <div className="h-[49px] w-[49px] object-cover rounded-full md:h-[128px] md:w-[128px] overflow-hidden ">
         <img
           src={el?.logo ? generateFilePath(el?.logo) : dummyBrand}
           alt="brand_logo"
